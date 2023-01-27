59da966f49a8b9de4069ce869771c902d60ca615
Yacine FODIL <yfodil@scaleway.com>
2023-01-27 13:40:09 +0100

    add rdb namespace

---
+++ config/rdb/config.go
@@ -0,0 +1,88 @@
+package rdb
+
+import "github.com/upbound/upjet/pkg/config"
+
+const shortGroup = "rdb"
+
+// Configure adds configurations for rdb resources.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("scaleway_rdb_acl", func(r *config.Resource) {
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = shortGroup
+		r.Kind = "ACL"
+
+		r.References["instance_id"] = config.Reference{
+			Type: "Instance",
+		}
+	})
+
+	p.AddResourceConfigurator("scaleway_rdb_database", func(r *config.Resource) {
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = shortGroup
+		r.Kind = "Database"
+
+		r.References["instance_id"] = config.Reference{
+			Type: "Instance",
+		}
+	})
+
+	p.AddResourceConfigurator("scaleway_rdb_database_backup", func(r *config.Resource) {
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = shortGroup
+		r.Kind = "DatabaseBackup"
+
+		r.References["instance_id"] = config.Reference{
+			Type: "Instance",
+		}
+	})
+
+	p.AddResourceConfigurator("scaleway_rdb_instance", func(r *config.Resource) {
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = shortGroup
+		r.Kind = "Instance"
+	})
+
+	p.AddResourceConfigurator("scaleway_rdb_privilege", func(r *config.Resource) {
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = shortGroup
+		r.Kind = "Privilege"
+
+		r.References["instance_id"] = config.Reference{
+			Type: "Instance",
+		}
+	})
+
+	p.AddResourceConfigurator("scaleway_rdb_read_replica", func(r *config.Resource) {
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = shortGroup
+		r.Kind = "ReadReplica"
+
+		r.References["instance_id"] = config.Reference{
+			Type: "Instance",
+		}
+	})
+
+	p.AddResourceConfigurator("scaleway_rdb_user", func(r *config.Resource) {
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = shortGroup
+		r.Kind = "User"
+
+		r.References["instance_id"] = config.Reference{
+			Type: "Instance",
+		}
+	})
+}
