bf4bf81a6cce57c1dc707e308f02d126e4141333
Yacine FODIL <yfodil@scaleway.com>
2023-10-05 17:52:48 +0200

    lint

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -8,8 +8,9 @@ import (
 	"context"
 	"os"
 	"path/filepath"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
