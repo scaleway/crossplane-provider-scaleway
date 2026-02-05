eb78d8c4a6c2ebfd1002612041d0f00495a3450f
Yacine FODIL <yfodil@scaleway.com>
2026-02-05 16:46:30 +0100

    trigger ci

--- config/domain/config.go
+++ config/domain/config.go
@@ -7,8 +7,6 @@ const shortGroup = "domain"
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("scaleway_domain_record", func(r *config.Resource) {
-		// Identifier for this resource is assigned by the provider. In other
-		// words it is not simply the name of the resource.
 		r.ExternalName = config.IdentifierFromProvider
 		r.ShortGroup = shortGroup
 		r.Kind = "Record"
@@ -19,8 +17,6 @@ func Configure(p *config.Provider) {
 	})
 
 	p.AddResourceConfigurator("scaleway_domain_zone", func(r *config.Resource) {
-		// Identifier for this resource is assigned by the provider. In other
-		// words it is not simply the name of the resource.
 		r.ExternalName = config.IdentifierFromProvider
 		r.ShortGroup = shortGroup
 		r.Kind = "Zone"
